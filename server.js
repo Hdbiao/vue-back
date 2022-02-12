09ecfeb5bf60081c25878315b908ef1c47bc809e
huangdongbiao <dongbiao.huang@zhenai.com>
2022-02-12 15:00:27 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http')
+
+const users = [
+    { id: 1, name: 'lilei' },
+    { id: 2, name: 'hanmeimei' },
+    { id: 3, name: 'yuanhua' },
+]
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    if (req.url === '/api/users') {
+        res.end(JSON.stringify(users))
+    } else {
+        res.end('Not Found')
+    }
+})
+
+server.listen(3000, () => {
+    console.log('服务启动到3000端口');
+})
